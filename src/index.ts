faff93cb4052df507f0841dcbe06dd1e0f90b114
StevenJBurns <stevenjburns@msn.com>
2021-03-21 23:18:53 -0600

    refactor: move server as a function to its own file

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import { server } from './server';
 
 dotenv.config();
 
-const port = process.env.PORT ?? 4000;
+const port = process.env.SERVER_PORT ?? 5000;
 const app = server();
 
 app.listen(port, () => {
   console.clear();
-  console.log(`Server Running on Port ${port}`);
+  console.log(`Server Running on Port ${port}\n`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,22 @@
 import express from 'express';
+import graphqlHTTP from 'express-graphql';
+// import schema from './graphql/schema/root';
 
 export const server = () => {
   const app = express();
+
+  app.use(express.json());
   
-  app.all('*', (req, res) => res.send(`Request Recieved: ${req.method}`));
+  // app.use('/graphql', graphqlHTTP({ schema, graphiql: true }));
+  app.all('*', (req, res) => {
+    console.log(`Request Recieved: ${req.method}`);
+    console.log(`Request Recieved: ${req.url}`);
+    res.send(`Request Recieved: ${req.method}`);
+  });
+
+  app.all('*', (req, res) => {
+    res.send(`Request Port: ${req.url}`)
+  });
 
   return app;
 };
